28465a959ecd83d9247443068cd060e77a8fccd9
takayoshi.naka <naka87003@outlook.jp>
2023-11-05 16:51:39 +0900

    inertia-vue.js

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue';
+import InputLabel from '@/Components/InputLabel.vue'
+import TextInput from '@/Components/TextInput.vue'
+import GuestLayout from '@/Layouts/GuestLayout.vue'
+
+const aaa = ref('こんにちは');
+</script>
+<template>
+    <GuestLayout>
+        <InputLabel value="タイトル"></InputLabel>
+        <TextInput v-model="aaa"></TextInput>
+    </GuestLayout>
+</template>
