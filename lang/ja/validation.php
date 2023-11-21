33612c59fe554f49b3557ec8d482b56377d00040
takayoshi.naka <naka87003@outlook.jp>
2023-11-22 07:25:26 +0900

    customers

--- lang/ja/validation.php
+++ lang/ja/validation.php
@@ -159,6 +159,13 @@ return [
         'name' => '名前',
         'price' => '価格',
         'memo' => 'メモ',
+        'kana' => 'カナ',
+        'tel' => '電話番号',
+        'email' => 'メールアドレス',
+        'postcode' => '郵便番号',
+        'address' => '住所',
+        'birthday' => '誕生日',
+        'gender' => '性別'
     ],
 
 ];
---
+++ resources/js/Pages/Customers/Create.vue
@@ -0,0 +1,126 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+import { ref } from 'vue'
+import { Inertia } from '@inertiajs/inertia'
+import InputError from '@/Components/InputError.vue';
+import { Core as YubinBangoCore } from "yubinbango-core2";
+
+const props = defineProps({
+  errors: Object
+});
+const form = ref({
+  name: null, kana: null, tel: null, email: null, postcode: null,
+  address: null, birthday: null, gender: '2', memo: null
+});
+const storeCustomer = () => {
+  Inertia.post('/customers', form.value)
+};
+// 数字を文字に変換 第１引数が郵便番号、第２がコールバックで引数に住所
+const fetchAddress = () => {
+  new YubinBangoCore(String(form.value.postcode), (value) => {
+    form.value.address = value.region + value.locality + value.street
+  })
+}
+</script>
+
+<template>
+  <Head title="顧客登録" />
+  <AuthenticatedLayout>
+    <template #header>
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">顧客登録</h2>
+    </template>
+    <section class="text-gray-600 body-font relative">
+      <form @submit.prevent="storeCustomer">
+        <div class="container px-5 py-8 mx-auto">
+          <div class="lg:w-1/2 md:w-2/3 mx-auto">
+            <div class="flex flex-wrap -m-2">
+              <div class="p-2 w-full">
+                <div class="relative">
+                  <label for="name" class="leading-7 text-sm text-gray-600">名前</label>
+                  <input type="text" v-model="form.name" id="name" name="name"
+                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                  <InputError :message="props.errors.name" />
+                </div>
+              </div>
+              <div class="p-2 w-full">
+                <div class="relative">
+                  <label for="kana" class="leading-7 text-sm text-gray-600">カナ</label>
+                  <input type="text" v-model="form.kana" id="kana" name="kana"
+                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                  <InputError :message="props.errors.kana" />
+                </div>
+              </div>
+              <div class="p-2 w-full">
+                <div class="relative">
+                  <label for="tel" class="leading-7 text-sm text-gray-600">電話番号</label>
+                  <input type="tel" v-model="form.tel" id="tel" name="tel"
+                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                  <InputError :message="props.errors.tel" />
+                </div>
+              </div>
+              <div class="p-2 w-full">
+                <div class="relative">
+                  <label for="email" class="leading-7 text-sm text-gray-600">メールアドレス</label>
+                  <input type="email" v-model="form.email" id="email" name="email"
+                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                  <InputError :message="props.errors.email" />
+                </div>
+              </div>
+              <div class="p-2 w-full">
+                <div class="relative">
+                  <label for="postcode" class="leading-7 text-sm text-gray-600">郵便番号</label>
+                  <input type="number" v-model="form.postcode" id="postcode" name="postcode"
+                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                    @input="fetchAddress">
+                  <InputError :message="props.errors.postcode" />
+                </div>
+              </div>
+              <div class="p-2 w-full">
+                <div class="relative">
+                  <label for="address" class="leading-7 text-sm text-gray-600">住所</label>
+                  <input type="text" v-model="form.address" id="address" name="address"
+                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                  <InputError :message="props.errors.address" />
+                </div>
+              </div>
+              <div class="p-2 w-full">
+                <div class="relative">
+                  <label for="birthday" class="leading-7 text-sm text-gray-600">誕生日</label>
+                  <input type="date" v-model="form.birthday" id="birthday" name="birthday"
+                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                  <InputError :message="props.errors.birthday" />
+                </div>
+              </div>
+              <div class="p-2 w-full">
+                <div class="relative">
+                  <label class="leading-7 text-sm text-gray-600">性別</label>
+                  <div>
+                    <input id="gender_0" type="radio" name="gender" v-model="form.gender" value="0">
+                    <label for="gender_0" class="mr-2">男性</label>
+                    <input id="gender_1" type="radio" name="gender" v-model="form.gender" value="1">
+                    <label for="gender_1" class="mr-2">女性</label>
+                    <input id="gender_2" type="radio" name="gender" v-model="form.gender" value="2">
+                    <label for="gender_2" class="mr-2">非公開</label>
+                  </div>
+                </div>
+              </div>
+              <div class="p-2 w-full">
+                <div class="relative">
+                  <label for="memo" class="leading-7 text-sm text-gray-600">メモ</label>
+                  <textarea v-model="form.memo" id="memo" name="memo"
+                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                  <InputError :message="props.errors.memo" />
+                </div>
+              </div>
+              <div class="p-2 w-full">
+                <button
+                  class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">顧客登録</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </section>
+  </AuthenticatedLayout>
+</template>
